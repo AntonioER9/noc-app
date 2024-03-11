5e8546fa8bcfc9417116a2d5ea5b9388ac48f667
Antonio Espinoza <aespinozareyes9@gmail.com>
2024-03-11 10:11:45 -0300

    add email notification

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -1,21 +1,34 @@
+import { envs } from '../config/plugins/envs.plugin';
 import { CheckService } from '../domain/use-cases/checks/check-service';
+import { SendEmailLogs } from '../domain/use-cases/email/send-email-logs';
 import { FileSystemDatasource } from '../infrastructure/datasources/file-system.datasource';
 import { LogRepositoryImpl } from '../infrastructure/repositories/log.repository.impl';
 import { CronService } from './cron/cron-service';
+import { EmailService } from './email/email.service';
+
 
 const fileSystemLogRepository = new LogRepositoryImpl(
   new FileSystemDatasource(),
-	//postgress
-	//mongo
 );
+const emailService = new EmailService();
 
 export class Server {
 
   public static start() {
 
-    console.log( 'Server started...' );
+    console.log('Server started...');
 
-    
+    //todo: Mandar email
+    // new SendEmailLogs(
+    //   emailService, 
+    //   fileSystemLogRepository,
+    // ).execute(
+    //   ['aespinozareyes9@gmail.com']
+    // )
+    // emailService.sendEmailWithFileSystemLogs(
+    //   ['aespinozareyes9@gmail.com']
+    // );
+
     // CronService.createJob(
     //   '*/5 * * * * *',
     //   () => {
@@ -26,11 +39,10 @@ export class Server {
     //       ( error ) => console.log( error ),
     //     ).execute( url );
     //     // new CheckService().execute( 'http://localhost:3000' );
-        
+
     //   }
     // );
 
   }
 
 }
-

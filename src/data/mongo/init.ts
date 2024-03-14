58306193343304a26095f6217f43cdd6d9f568f2
Antonio Espinoza <aespinozareyes9@gmail.com>
2024-03-14 10:02:19 -0300

    add test in init mongo db

--- src/data/mongo/init.ts
+++ src/data/mongo/init.ts
@@ -16,15 +16,12 @@ export class MongoDatabase {
       await mongoose.connect(mongoUrl, {
         dbName: dbName,
       });
-
-      console.log('Mongo connected!');
-
+      // console.log('Mongo connected!');
+      return true
 
     } catch (error) {
-      console.log('Mongo connection error');
+      // console.log('Mongo connection error');
       throw error;
     }
-
   }
-
 }
